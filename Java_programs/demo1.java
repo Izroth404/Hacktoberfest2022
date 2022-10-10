36a9e6cc710fe6a59acc6851c0d56237b4feacde
Izroth404 <ranaavirup@gmail.com>
2022-10-10 21:30:38 +0530

    2nd Commit Island Problem

--- Java_programs/demo1.java
+++ Java_programs/demo1.java
@@ -1,4 +1,5 @@
-import java.util.*;
+package Hacktoberfest2022.Java_programs;
+// import java.util.*;
 class demo1{
     public static void main(String[] args) {
         System.out.println("Hello World");
